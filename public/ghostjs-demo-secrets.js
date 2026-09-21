8ce8dae14b2b458839cb19e66853d86d6bf5f8d5
Bharatkushwah02 <bv6226780@gmail.com>
2026-09-21 14:24:49 +0530

    Add huggingfaceToken to ghostjs-demo-secrets.js

--- public/ghostjs-demo-secrets.js
+++ public/ghostjs-demo-secrets.js
@@ -3,4 +3,5 @@ window.__GHOSTJS_TRAINING_FIXTURES__ = {
   geminiApiKey: "AQ.Ab8RN6LgzBWtpydrc9a1mhJGTuamwYXzLJ261NeYnKdNQzFYXA",
   openaiApiKey: "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",
   githubToken: "ghp_DEMO_NOT_A_REAL_TOKEN"
+  huggingfaceToken: "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"
 };
